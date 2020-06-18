8c5aedf1f1f518c0db4244c75ef54c5effb02703
varunmehrotra2998 <varunmehrotra2998@gmail.com>
2020-06-18 11:06:31 +0530

    Login Page

---
+++ src/rav/JoptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rav;
+
+/**
+ *
+ * @author Varun Mehrotra
+ */
+class JoptionPane {
+
+    static void showMessageDialog(Object object, String error_while_established_connection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
